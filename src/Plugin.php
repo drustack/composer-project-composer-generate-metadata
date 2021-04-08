3b70d37dc360f18b25dcc47435ba4e137a8a2db5
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-04-08 13:30:07 +0800

    https://git.drupalcode.org/project/composer_deploy/-/blob/8.x-1.x/composer_deploy.module

--- src/Plugin.php
+++ src/Plugin.php
@@ -14,20 +14,13 @@ use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Repository\ComposerRepository;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Process\Process;
 
 /**
  * Class Plugin.
  */
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
-    /**
-     * The expected Drupal core version.
-     */
-    protected $core;
-
     /**
      * @var \Composer\Composer
      */
@@ -44,25 +37,28 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     protected $installationManager;
 
     /**
+     * @var string
+     */
+    protected $version;
+
+    /**
+     * @var string
+     */
+    protected $project;
+
+    /**
+     * @var string
+     */
+    protected $datastamp;
+
+    /**
      * {@inheritdoc}
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $this->io = $io;
         $this->composer = $composer;
+        $this->io = $io;
         $this->installationManager = $composer->getInstallationManager();
-
-        // Check Drupal core version.
-        $repositories = $composer->getRepositoryManager()->getRepositories();
-        foreach ($repositories as $repository) {
-            if ($repository instanceof ComposerRepository) {
-                $repoConfig = $repository->getRepoConfig();
-                if (preg_match('/packages\.drupal\.org\/([0-9]*)$/', $repoConfig['url'], $matches)) {
-                    $this->core = $matches[1];
-                    break;
-                }
-            }
-        }
     }
 
     /**
@@ -101,140 +97,45 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $package = ($op instanceof InstallOperation)
             ? $op->getPackage()
             : $op->getTargetPackage();
-        $installPath = $this->installationManager->getInstallPath($package);
+        $extra = $package->getExtra();
 
         if (preg_match('/^drupal-/', $package->getType())) {
             if (preg_match('/^dev-/', $package->getPrettyVersion())) {
-                $name = $package->getName();
-                $project = preg_replace('/^.*\//', '', $name);
-                $version = preg_replace('/^dev-(.*)/', $this->core.'.x-$1-dev', $package->getPrettyVersion());
-                $branch = preg_replace('/^([0-9]*\.x-[0-9]*).*$/', '$1', $version);
-                $datestamp = time();
-
-                $this->io->write('  - Generating metadata for <info>'.$name.'</info>');
-
                 // Compute the rebuild version string for a project.
-                $version = $this->computeRebuildVersion($installPath, $branch) ?: $version;
+                $this->project = preg_replace('/^drupal\//', '', $package->getName());
+                $this->version = $extra['drupal']['version']
+                    ?: preg_replace('/^dev-(.*)/', '$1-dev', $package->getPrettyVersion());
+                $this->datestamp = $extra['drupal']['datestamp']
+                    ?: time();
 
-                if ('7' == $this->core) {
-                    // Generate version information for `.info` files in ini format.
-                    $finder = new Finder();
-                    $finder
-                        ->files()
-                        ->in($installPath)
-                        ->name('*.info')
-                        ->notContains('datestamp =');
-                    foreach ($finder as $file) {
-                        // Remove `version` and `project` lines.
-                        file_put_contents(
-                            $file->getRealpath(),
-                            preg_replace(
-                                '/^\s*(version|project)\s*=.*\n/m',
-                                '',
-                                file_get_contents($file->getRealpath())
-                            )
-                        );
-                        // Append generated version information.
-                        file_put_contents(
-                            $file->getRealpath(),
-                            $this->generateInfoIniMetadata($version, $project, $datestamp),
-                            FILE_APPEND
-                        );
-                    }
-                } else {
-                    // Generate version information for `.info.yml` files in YAML format.
-                    $finder = new Finder();
-                    $finder
-                        ->files()
-                        ->in($installPath)
-                        ->name('*.info.yml')
-                        ->notContains('datestamp:');
-                    foreach ($finder as $file) {
-                        // Remove `version` and `project` lines.
-                        file_put_contents(
-                            $file->getRealpath(),
-                            preg_replace(
-                                '/^\s*(version|project)\s*:.*\n/m',
-                                '',
-                                file_get_contents($file->getRealpath())
-                            )
-                        );
-                        // Append generated version information.
-                        file_put_contents(
-                            $file->getRealpath(),
-                            $this->generateInfoYamlMetadata($version, $project, $datestamp),
-                            FILE_APPEND
-                        );
-                    }
+                $this->io->write('  - Generating metadata for <info>'.$this->project.'</info>');
+
+                // Generate version information for `.info.yml` files in YAML format.
+                $finder = new Finder();
+                $finder
+                    ->files()
+                    ->in($this->installationManager->getInstallPath($package))
+                    ->name('*.info.yml')
+                    ->notContains('datestamp:');
+                foreach ($finder as $file) {
+                    // Remove `version` and `project` lines.
+                    file_put_contents(
+                        $file->getRealpath(),
+                        preg_replace(
+                            '/^\s*(version|project)\s*:.*\n/m',
+                            '',
+                            file_get_contents($file->getRealpath())
+                        )
+                    );
+                    // Append generated version information.
+                    file_put_contents(
+                        $file->getRealpath(),
+                        $this->generateInfoYamlMetadata(),
+                        FILE_APPEND
+                    );
                 }
             }
         }
-    }
-
-    /**
-     * Helper function to compute the rebulid version string for a project.
-     *
-     * This does some magic in Git to find the latest release tag along
-     * the branch we're packaging from, count the number of commits since
-     * then, and use that to construct this fancy alternate version string
-     * which is useful for the version-specific dependency support in Drupal
-     * 7 and higher.
-     *
-     * NOTE: A similar function lives in git_deploy and in the drupal.org
-     * packaging script (see DrupalorgProjectPackageRelease.class.php inside
-     * drupalorg/drupalorg_project/plugins/release_packager). Any changes to the
-     * actual logic in here should probably be reflected in the other places.
-     *
-     * @see drush_pm_git_drupalorg_compute_rebuild_version()
-     */
-    protected function computeRebuildVersion($installPath, $branch)
-    {
-        $version = '';
-        $branchPreg = preg_quote($branch);
-
-        $process = new Process("cd $installPath; git describe --tags");
-        $process->run();
-        if ($process->isSuccessful()) {
-            $lastTag = strtok($process->getOutput(), "\n");
-            // Make sure the tag starts as Drupal formatted (for eg.
-            // 7.x-1.0-alpha1) and if we are on a proper branch (ie. not master)
-            // then it's on that branch.
-            if (preg_match('/^(?<drupalversion>'.$branchPreg.'\.\d+(?:-[^-]+)?)(?<gitextra>-(?<numberofcommits>\d+-)g[0-9a-f]{7})?$/', $lastTag, $matches)) {
-                if (isset($matches['gitextra'])) {
-                    // If we found additional git metadata (in particular, number of commits)
-                    // then use that info to build the version string.
-                    $version = $matches['drupalversion'].'+'.$matches['numberofcommits'].'dev';
-                } else {
-                    // Otherwise, the branch tip is pointing to the same commit as the
-                    // last tag on the branch, in which case we use the prior tag and
-                    // add '+0-dev' to indicate we're still on a -dev branch.
-                    $version = $lastTag.'+0-dev';
-                }
-            }
-        }
-
-        return $version;
-    }
-
-    /**
-     * Generate version information for `.info` files in ini format.
-     *
-     * @see _drush_pm_generate_info_ini_metadata()
-     */
-    protected function generateInfoIniMetadata($version, $project, $datestamp)
-    {
-        $core = preg_replace('/^([0-9]).*$/', '$1.x', $version);
-        $date = date('Y-m-d', $datestamp);
-        $info = <<<METADATA
-
-; Information add by drustack/composer-generate-metadata on {$date}
-version = "{$version}"
-core = "{$core}"
-project = "{$project}"
-datestamp = "{$datestamp}"
-METADATA;
-
-        return $info;
     }
 
     /**
@@ -242,16 +143,16 @@ METADATA;
      *
      * @see _drush_pm_generate_info_yaml_metadata()
      */
-    protected function generateInfoYamlMetadata($version, $project, $datestamp)
+    protected function generateInfoYamlMetadata()
     {
-        $core = preg_replace('/^([0-9]).*$/', '$1.x', $version);
-        $date = date('Y-m-d', $datestamp);
+        $date = date('Y-m-d', $this->datestamp);
         $info = <<<METADATA
 
 # Information add by drustack/composer-generate-metadata on {$date}
-project: "{$project}"
-version: "{$version}"
-datestamp: "{$datestamp}"
+version: "{$this->version}"
+project: "{$this->project}"
+datestamp: "{$this->datestamp}"
+
 METADATA;
 
         return $info;
